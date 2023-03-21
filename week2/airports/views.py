284ed5ddd2f7930818d01f44dda70a0770d645eb
Erinn Szarek <98053304+eszarek@users.noreply.github.com>
2023-03-21 16:11:58 -0400

    Week 9 Assignment- Displaying Data templates

--- week2/airports/views.py
+++ week2/airports/views.py
@@ -1,15 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Airport
+from django.shortcuts import render
 
 
 def index(request):
     airports = Airport.objects.all()
-    airport_list = ', '.join([a.airport_code for a in airports])
-    return HttpResponse('Showing all airports: ' + airport_list);
+    context= {'airports' : airports}
+    return render(request, 'airports/index.html', context)
+    #airport_list = ', '.join([a.airport_code for a in airports])
+    #return HttpResponse('Showing all airports: ' + airport_list);
 
 
 # Create your views here.
 def airport_info(request, airport_code):
     airport = Airport.objects.get(airport_code=airport_code)
-    return HttpResponse('Showing info for airport: ' + airport.name + '- ' + airport.airport_code)
+    context = {'airport': airport}
+    return render(request, 'airports/airport.html', context)
+    #airport = Airport.objects.get(airport_code=airport_code)
+    #return HttpResponse('Showing info for airport: ' + airport.name + '- ' + airport.airport_code)
